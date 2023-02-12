9829eb2f33bbf246ba8f67a4c5da7eebf14911c3
MohsenKondori <MohsenKondori@yahoo.com>
2023-02-12 14:20:11 +0330

    Create a simple example with empty window.

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -1,9 +1,19 @@
-#include <QGuiApplication>
+#include <QtGui/QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
   QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  const QUrl url(u"qrc:/QML/main.qml"_qs);
+  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                   &app, [url](QObject *obj, const QUrl &objUrl) {
+      if (!obj && url == objUrl)
+          QCoreApplication::exit(-1);
+  }, Qt::QueuedConnection);
+  engine.load(url);
+
   return app.exec();
 }
 
---
+++ examples/simple/main.qml
@@ -0,0 +1,9 @@
+import QtQuick
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Simple NodeLink Example")
+    color: "#5ad5ed"
+}
