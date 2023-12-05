3c2aac7e5d248940d012119611b5e2d099be39e4
MohsenKondori <MohsenKondori@yahoo.com>
2023-12-05 11:11:04 +0330

    feat: Create an arrowhead at the midpoint of a line (LLine, Line and Bezier curves).

--- resources/View/Logics/LinkPainter.js
+++ resources/View/Logics/LinkPainter.js
@@ -60,6 +60,7 @@ function createLink(context, startPos, controlPoints,
     switch (direction) {
     case 1: { // Unidirectional
         arrow(context, controlPoints[(startPosCP === startPos) ? controlPoints.length - 1 : 0], outputPortSide, color, arrowHeadLength);
+        arrows(context, controlPoints, color, arrowHeadLength, startPos, type)
         break;
     }
 
@@ -74,15 +75,79 @@ function createLink(context, startPos, controlPoints,
     }
 }
 
-// Code to draw a simple arrow on TypeScript canvas got from https://stackoverflow.com/a/64756256/867349
+//! Prepare variables to draw between line arrows
+function arrows(context, controlPoints, color, headLength, startPos, type) {
+    if (controlPoints.length === 0)
+        return;
+
+    var reverseDirection = (startPos === controlPoints[0]);
+    var margin = 10 + headLength;
+
+    // Draw Arrow for bezier curve
+    if (type !== 1) {
+        if (controlPoints.length < 4)
+            return;
+
+        margin = 50;
+
+        var endPos = controlPoints[1];
+        var startPosCp = controlPoints[2];
+
+        // if (reverseDirection)
+            // [endPos, startPosCp] = [startPosCp, endPos];
+
+       var targetPoint = findMidPoint(startPosCp, endPos);
+
+        var dx = startPos.x - controlPoints[3].x;
+        var dy = startPos.y - controlPoints[3].y;
+
+        // Calculate angle using atan2 function
+        var angle = calculateAngle(startPosCp, endPos);
+
+        console.log(margin, Math.abs(dx), Math.abs(dy), reverseDirection);
+        if (Math.abs(dx) > margin || Math.abs(dy) > margin)
+          drawArrow(context, targetPoint, angle, color, headLength);
+
+        return;
+    }
+
+    // Draw Arrows for line and L line.
+    controlPoints.forEach((cp, index) => {
+                              if (controlPoints.length <= index + 1)
+                                return;
+
+                              var endPos = controlPoints[index + 1]
+                              var startPosCp = cp;
+
+                              if (reverseDirection)
+                                [endPos, startPosCp] = [startPosCp, endPos];
+
+                              var targetPoint = findMidPoint(startPosCp, endPos);
+
+                              var dx = startPosCp.x - endPos.x;
+                              var dy = startPosCp.y - endPos.y;
+
+                              // Calculate angle using atan2 function
+                              var angle = Math.atan2(dy, dx);
+
+                              if (Math.abs(dx) > margin || Math.abs(dy) > margin )
+                                drawArrow(context, targetPoint, angle, color, headLength)
+                          });
+}
+
+//! Prepare variables to draw arrow at the start and end point
 function arrow(context, targetPoint, portSide, color, arrowHeadLength) {
     const dx = arrowAngle(portSide).x;
     const dy = arrowAngle(portSide).y;
 
-    const headlen = arrowHeadLength; // length of head in pixels
     // Calculate Arrow angle
     const angle = Math.atan2(dy, dx);
 
+    drawArrow(context, targetPoint, angle, color, arrowHeadLength);
+}
+
+//! Code to draw a simple arrow on TypeScript canvas got from https://stackoverflow.com/a/64756256/867349
+function drawArrow(context, targetPoint, angle, color, headlen) {
     //Paint arrow
     context.beginPath();
     context.moveTo(targetPoint.x - headlen * Math.cos(angle - Math.PI / 6), targetPoint.y - headlen * Math.sin(angle - Math.PI / 6));
@@ -99,6 +164,25 @@ function arrow(context, targetPoint, portSide, color, arrowHeadLength) {
 
     // stroke the curve
     context.stroke();
+}
+
+//! Locate the midpoint between startPos and endPos.
+function findMidPoint(startPos, endPos) {
+    var midX = (startPos.x + endPos.x) / 2;
+    var midY = (startPos.y + endPos.y) / 2;
+
+    return Qt.vector2d(midX, midY);
+}
+
+//! Calculate the angle formed by two points.
+function calculateAngle(startPos, endPos) {
+    var dx = startPos.x - endPos.x;
+    var dy = startPos.y - endPos.y;
+
+    // Calculate angle using atan2 function
+    var angle = Math.atan2(dy, dx) ;
+
+    return angle;
 }
 
 //! Paint curve line
