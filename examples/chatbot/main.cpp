31d57e2f0c643b808fd7ef2b9158a6ee3f52fe5e
YasinFaraji <yasinfaraji100@gmail.com>
2025-11-03 21:27:50 +0330

    Add main.cpp file for chatbot example

---
+++ examples/chatbot/main.cpp
@@ -0,0 +1,22 @@
+#include <QtGui/QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char* argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    QQuickStyle::setStyle("Material");
+    engine.addImportPath(":/");
+
+    const QUrl url(u"qrc:/ChatBot/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+    return app.exec();
+}
