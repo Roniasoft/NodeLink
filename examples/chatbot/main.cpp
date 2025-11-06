a498eea49aae4db0c0ce97a511b71ba49d8d7fae
YasinFaraji <yasinfaraji100@gmail.com>
2025-11-06 10:52:05 +0330

    Update name of example to main.cpp

--- examples/chatbot/main.cpp
+++ examples/chatbot/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     QQuickStyle::setStyle("Material");
     engine.addImportPath(":/");
 
-    const QUrl url(u"qrc:/ChatBot/main.qml"_qs);
+    const QUrl url(u"qrc:/Chatbot/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
